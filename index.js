353987b082f1dd9756a8ccb95f1a9b8f4ce2b653
herrera396 <gabriel.herrera@gloablhitss.com.br>
2024-05-09 13:10:44 -0300

    new branch

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen("3000", ()=>{
-    console.log("Server is listening on port 3000")
+app.listen("3009", ()=>{
+    console.log("Server is listening on port 3009")
 })
